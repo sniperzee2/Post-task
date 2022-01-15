9d6713aa1d41dd38bd595411ad72833ad66c0ecf
Shivansh Joshi <77457296+sniperzee2@users.noreply.github.com>
2022-01-15 17:04:00 +0530

    Loader

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect} from "react";
 import axios from "axios";
 import Pagination from "./components/Pagination/Pagination";
-import Post from "./components/Posts/Post";
+import Post from "./components/post/Post";
 import "./App.css";
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [filteredPost, setFilteredPost] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(async () => {
     try {
+      setLoading(true);
       const response = await axios.get(
         "https://jsonplaceholder.typicode.com/posts"
       );
+      setLoading(false);
       setPosts(response.data);
       setFilteredPost(response.data);
     } catch (err) {
+      setLoading(false);
       console.log(err);
       throw new Error("something went wrong");
     }
@@ -29,17 +33,21 @@ function App() {
     );
   };
 
+  if(loading) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <>
       <div className="header">
         <div style={{ marginLeft: "1rem" }}>
           <h1>Posts</h1>
         </div>
-        <div className="wrap">
-          <div className="search">
-            <input type="text" className="searchTerm" placeholder="What are you looking for?" onChange={searchPost} />
-            <button type="submit" className="searchButton">
-              <i className="fa fa-search"></i>
+        <div class="wrap">
+          <div class="search">
+            <input type="text" class="searchTerm" placeholder="What are you looking for?" onChange={searchPost} />
+            <button type="submit" class="searchButton">
+              <i class="fa fa-search"></i>
             </button>
           </div>
         </div>
